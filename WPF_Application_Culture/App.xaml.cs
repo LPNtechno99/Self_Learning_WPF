ac675adceeba6fcae6b47a2bb28718ad5cb7ae51
LPNtechno99 <lpntechno99@gmail.com>
2020-08-20 17:15:12 +0700

    Cap nhat ngay 20/08/2020

---
+++ WPF_Application_Culture/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_Application_Culture
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("de-DE");
+        }
+    }
+}
---
+++ WPF_Handling_Exception/HandlingException.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Handling_Exception
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class HandlingException : Window
+    {
+        public HandlingException()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClic_Click(object sender, RoutedEventArgs e)
+        {
+            string s = null;
+            try
+            {
+                s.Trim();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Handled Exception: " + ex.Message, "Exception example", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+            }
+        }
+    }
+}
