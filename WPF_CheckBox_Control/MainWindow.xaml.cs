88aa1263ac7adacffe95caf2750b98cf4508a405
LPNtechno99 <lpntechno99@gmail.com>
2020-08-21 17:26:48 +0700

    Cap nhat ngay 21/08/2020

---
+++ WPF_CheckBox_Control/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_CheckBox_Control
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CbAllFeature_Checked(object sender, RoutedEventArgs e)
+        {
+            bool newVal = (cbAllFeature.IsChecked == true);
+            cbFeatureAbc.IsChecked = newVal;
+            cbFeatureXyz.IsChecked = newVal;
+            cbFeatureWww.IsChecked = newVal;
+        }
+        private void cbFeature_CheckedChanged(object sender, RoutedEventArgs e)
+        {
+            cbAllFeature.IsChecked = null;
+            if ((cbFeatureAbc.IsChecked == true) && (cbFeatureXyz.IsChecked == true) && (cbFeatureWww.IsChecked == true))
+                cbAllFeature.IsChecked = true;
+            if ((cbFeatureAbc.IsChecked == false) && (cbFeatureXyz.IsChecked == false) && (cbFeatureWww.IsChecked == false))
+                cbAllFeature.IsChecked = false;
+        }
+    }
+}
